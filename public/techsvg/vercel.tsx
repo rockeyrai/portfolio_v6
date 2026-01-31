a6b51b55d14bd9d241a0767c6697927349db65a4
rockeyrai <rockeyrai234@gmail.com>
2026-01-31 12:10:46 +0545

    adding the need svg of teach stack use

---
+++ public/techsvg/vercel.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+const VercelSvg = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={64}
+    height={64}
+    fill="none"
+    {...props}
+  >
+    <path fill="#000" d="m31.991 4 31.992 55.411H0z" />
+  </svg>
+);
+export default VercelSvg;
